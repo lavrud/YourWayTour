b6956978efe9ab51d0fd624a1fbc2a6eea3dbd33
Lavrud <ohndoe@example.com>
2017-11-28 19:48:59 -0200

    finalização header home_page

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require("gulp-uglify");
 var watch = require('gulp-watch');
 // task sass renderiza:css/comprimir
 gulp.task('sass', function () {
@@ -7,9 +8,13 @@ gulp.task('sass', function () {
         outputStyle: 'compressed'
     }).on('error', sass.logError)).pipe(gulp.dest('css'));
 });
+// task minify comprimir/js
+gulp.task('minify-js', function () {
+    gulp.src('js/**/*.js').pipe(uglify()).pipe(gulp.dest('js'));
+});
 // task watch assistir tarefas gulp
 gulp.task('watch', function () {
     gulp.watch('scss/**/*.scss', ['sass']);
 });
 // task padrão gulp
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'watch', 'minify-js']);
